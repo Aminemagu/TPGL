c3542b9a0b02df622de706b8e70680b251e37647
Amine <amine@MacBook-Pro-de-Amine.local>
2019-09-29 17:27:39 +0200

    premier commit

---
+++ orange.java
@@ -0,0 +1,27 @@
+public class orange {
+private double prix;
+private String origine;
+
+public orange(){
+this.prix = 0.0;
+this.origine = "indéfinie";
+}
+
+
+public double getPrix(){
+return this.prix;
+}
+public void setPrix(double prix){
+this.prix = prix;
+}
+public String getOrigine(){
+return this.origine;
+}
+public void setOrigine(String origine){
+this.origine = origine;
+
+}
+
+
+
+}
